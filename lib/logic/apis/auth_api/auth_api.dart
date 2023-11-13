989e0e426e7a51cd7825140bb64f84632f2aa34b
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-13 14:17:11 +0100

    Add custom page transition animations

--- lib/logic/apis/auth_api/auth_api.dart
+++ lib/logic/apis/auth_api/auth_api.dart
@@ -1,7 +1,14 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart' as model;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_twitter_clone/core/core.dart';
+import 'package:flutter_twitter_clone/core/providers.dart';
 import 'package:fpdart/fpdart.dart';
+
+final authAPIProvider = Provider((ref) {
+  final account = ref.watch(appwriteAccountProvider);
+  return AuthApi(account: account);
+});
 
 abstract class IAuthApi {
   FutureEither<model.User> signUp({
