b293a23a30e9c3281237ea3c02afe6d30b5e0dda
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-13 13:37:24 +0100

    Add some dependencies for future use

--- lib/logic/apis/auth_api/auth_api.dart
+++ lib/logic/apis/auth_api/auth_api.dart
@@ -1,11 +1,33 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
 import 'package:flutter_twitter_clone/core/core.dart';
+import 'package:fpdart/fpdart.dart';
 
 abstract class IAuthApi {
-  FutureEither<Account> signUp({
+  FutureEither<model.User> signUp({
     required String email,
     required String password,
   });
 }
 
-// class AuthApi implements IAuthApi {}
+class AuthApi implements IAuthApi {
+  final Account _account;
+  AuthApi({required Account account}) : _account = account;
+  @override
+  FutureEither<model.User> signUp(
+      {required String email, required String password}) async {
+    try {
+      final account = await _account.create(
+        userId: ID.unique(),
+        email: email,
+        password: password,
+      );
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+          Failure(e.message ?? 'An unexpected error occured', stackTrace));
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+    }
+  }
+}
