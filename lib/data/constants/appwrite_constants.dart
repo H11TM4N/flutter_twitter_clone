a750ec945e648ca79ea87cc5899638c6c6d7a67b
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-13 10:23:44 +0100

    first commit

---
+++ lib/data/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwrieConstants {
+  static const String databaseID = '6551d8ae987ed386565b';
+  static const String projectID = '6551d6a75938e838a2bd';
+  static const String endPoint = 'https://cloud.appwrite.io/v1';
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_twitter_clone/ui/theme/theme.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Client client = Client();
+  // client = Client()
+  //     .setEndpoint("https://cloud.appwrite.io/v1")
+  //     .setProject("650209298acac4ee1bb6");
+
+  // Account account = Account(client);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: AppTheme.theme,
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
