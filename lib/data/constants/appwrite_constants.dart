ec0d8de85ae767814336e3706d143336785641d1
JER3MIAH <jeremiahbrownie@gmail.com>
2023-11-14 16:13:56 +0100

    Remove a file from version control

--- lib/data/constants/appwrite_constants.dart
+++
@@ -1,5 +0,0 @@
-class AppwrieConstants {
-  static const String databaseID = '6552319ce14e01952ad9';
-  static const String projectID = '655230ab68821766d6d2';
-  static const String endPoint = 'https://cloud.appwrite.io/v1';
-}
